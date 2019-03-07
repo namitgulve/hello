98bacde6c99b0553feb39e11918db5964abd1b0c
namitgulve <namitgulve@gmail.com>
2019-03-07 15:05:00 +0530

    my project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+const routes: Routes = [
+  { path:'', component:AppComponent },
+  { path:'page2', component: Page2Component },
+  { path:'page1', component: Page1Component }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
